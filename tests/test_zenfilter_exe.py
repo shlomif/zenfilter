31a415aba2112444b2f809658b34ee7ab1d024e1
Shlomi Fish <shlomif@shlomifish.org>
2018-01-30 19:46:57 +0200

    Add a test for --filter.

--- tests/test_zenfilter_exe.py
+++ tests/test_zenfilter_exe.py
@@ -25,3 +25,9 @@ def _test_input_output(args, input_, expected_output, desc):
 def test_modint():
     _test_input_output(
         ['--last', '2'], b'1\n2\n3\n4\n', b'LAST\t3\nLAST\t4\n', 'test --last')
+    _test_input_output(
+        ['--last', '1', '--filter', 'my.*pat'],
+        b'1 my pattern\n2 my please\n3 my patently obvious\n' +
+        b'4 foo\n5 spam\n6 pink\n',
+        b'FOUND\t1 my pattern\nFOUND\t3 my patently obvious\nLAST\t6 pink\n',
+        'test --filter')
