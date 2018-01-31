54c4c4f4b2e0d945b399d3da55ceb978f5c58611
Shlomi Fish <shlomif@shlomifish.org>
2018-01-31 18:57:31 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='zenfilter',
-        version='0.2.0',
+        version='0.4.0',
         description='Filters TravisCI output',
         author='Shlomi Fish',
         author_email='shlomif@shlomifish.org',
