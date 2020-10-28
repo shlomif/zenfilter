acb495d83f9e77578805a8f4c46686349939de39
Shlomi Fish <shlomif@shlomifish.org>
2020-10-28 11:03:29 +0200

    start converting to pydistman

---
+++ code/README.part.rst
@@ -0,0 +1,31 @@
+PURPOSE
+-------
+
+This small script filters long STDIN output, performing several functions
+to keep track of the important parts and progresses, which will be hard to
+do with a shell script.
+
+It is useful for filtering the output of verbose
+Travis-CI ( https://travis-ci.org/ ) commands, but may be useful in other
+contexts where there is a limit to the amount of kept output.
+
+All arguments are optional:
+
+* `--count-step=n`: displays `COUNT <tab> <step>` every n lines.
+* `--last=n`: displays the last n lines prefixed with "LAST\t"
+* `--filter=<regex pattern>`: displays matching lines with a "FOUND\t" prefix.
+* `--suppress-last-on=<regex>`: suppress the last lines if their concatenated output matches the regex.
+
+Examples:
+
+::
+
+    python zenfilter.py --count-step=10 --last=200 --filter="[0-9]+"
+    python zenfilter.py --last=20
+    python zenfilter.py --last=25 --count-step=15
+
+A use case scenario:
+
+::
+
+    make 2>&1 | python zenfilter.py [args]
---
+++ python_pypi_dist_manager.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+
+import sys
+
+from pydistman import DistManager
+
+
+class Derived(DistManager):
+    def _build_only_command_custom_steps(self):
+        self._dest_append("zenfilter/zenfilter", make_exe=True)
+        for fn in ["{dest_dir}/setup.py", ]:
+            self._re_mutate(
+                fn_proto=fn,
+                pattern="include_package_data=True,",
+                repl_fn_proto=None,
+                prefix="include_package_data=True,\n    " +
+                "\n    scripts=['zenfilter/zenfilter'],",
+                )
+        self._dest_append("MANIFEST.in")
+
+
+try:
+    cmd = sys.argv.pop(1)
+except IndexError:
+    cmd = 'build'
+
+dist_name = "zenfilter"
+
+obj = Derived(
+    dist_name=dist_name,
+    dist_version="0.6.0",
+    project_name="zenfilter",
+    project_short_description="Filter stdin to avoid excessive output",
+    release_date="2020-10-28",
+    project_year="2016",
+    aur_email="shlomif@cpan.org",
+    project_email="shlomif@cpan.org",
+    full_name="Shlomi Fish",
+    github_username="shlomif",
+)
+obj.run_command(cmd=cmd, args=[])
